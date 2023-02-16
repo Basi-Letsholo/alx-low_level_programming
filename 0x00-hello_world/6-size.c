5a3b10bcec7f896e32fa8aac613612ea730e1e4e
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-16 09:23:51 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)\n", sizeof(float));
+	getch();
+	return (0);
+}
