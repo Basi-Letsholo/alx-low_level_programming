e97720725ac003a8a1d8229d7c7816dc7481665d
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-17 09:17:42 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints integers 0-9, using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(48 + n);
+	}
+	putchar('\n');
+	return (0);
+}
