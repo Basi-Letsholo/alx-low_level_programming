323ea69ef4d8d64b8e1539a7ccff138ce8ca0ffe
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-17 06:48:17 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,16 @@
  */
 int main(void)
 {
-	char alf;
-	for (alf = 'a'; alf <= 'z'; alf++)
+	char az;
+	char AZ;
 
+	for (az = 'a'; az <= 'z'; az++)
 	{
-		putchar(alf);
+		putchar(az);
+	}
+	for (AZ = 'A'; AZ <= 'Z'; AZ++)
+	{
+		putchar(AZ);
 	}
 	putchar('\n');
 	return (0);
