8d166b722a6a06a8cef8b8308b0bd46ee7285124
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-17 06:27:10 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alf;
+
+	for (alf = 'a'; alf <= 'z'; alf++)
+	{
+		putchar (alf);
+	}
+	putchar('\n');
+	return (0);
+}
