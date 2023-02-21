532fbb5ea562164a70b489fba0e04ea12ecb108e
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-21 03:06:23 -0800

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * description: prints _putchar
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+	return (0);
+}
