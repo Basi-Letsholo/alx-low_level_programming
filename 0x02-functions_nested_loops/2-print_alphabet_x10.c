068960f79e13980688c18236e859fe3420e2ed78
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-22 08:31:01 -0800

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: prints alphabet x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char az;
+	char i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+	_putchar('\n');
+	}
+}
