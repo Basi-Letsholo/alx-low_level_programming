f174e88f8a1941b5a75c467d382a57a9b4afa215
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-22 15:25:18 -0800

    fix 24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,29 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		if (a < 2)
 		{
-			for (c = 0; c <= 5; c++)
+			for (b = 0; b <= 9; b++)
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					printf("%d%d:%d%d\n", a, b, c, d);
+					for (d = 0; d <= 9; d++)
+					{
+						printf("%d%d:%d%d\n", a, b, c, d);
+					}
+				}
+			}
+		}
+		else if (a == 2)
+		{
+			for (b = 0; b <= 3; b++)
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						printf("%d%d:%d%d\n", a, b, c, d);
+					}
 				}
 			}
 		}
