7e0b82bf9d1afddd1bb9eb63d6300d20207a4163
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-23 05:19:24 -0800

    multiply this

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - entry point
+ *
+ * Description: multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
