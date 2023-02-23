ea6cf28d3d304aed3563b88f7a70058ce6f2a79f
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-23 10:51:45 -0800

    save

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - entry point
+ *
+ * Descriptions: prints triangle of height size
+ * @size: size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int k;
+	int l;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (k = size - i; k >= 1; k--)
+			{
+				_putchar(' ');
+			}
+			for (l = size - k; l <= size; l++)
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
