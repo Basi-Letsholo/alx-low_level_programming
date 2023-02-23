19de3437ea45f181a5247f5af36870ba2358c964
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-23 15:27:55 -0800

    sign fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * print_sign - entry point
- * description: prints sign of integer i.e positive or negative
+ * Description: prints sign of integer i.e positive or negative
  * @n: integer
- * return: 1 if positive, 0 if 0, or -1 if negative
+ * Return: 1 if positive, 0 if 0, or -1 if negative
  */
 
 int print_sign(int n)
 {
 	if (n == 0)
 	{
-		printf("0");
+		_putchar(48);
 		return (0);
 	}
 	else if (n > 0)
 	{
-		printf("+");
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		_putchar(45);
 		return (-1);
 	}
+	return (0);
 }
