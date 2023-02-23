0ec5c0f4c0a812a36d89dc4c2bb5b89ce41d176d
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-23 15:35:08 -0800

    fix 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,16 @@
 #include "main.h"
 /**
  * _abs - entry point
- * description: prints absolute value of each integer
- * @int: integer
+ * Description: prints absolute value of each integer
+ * @b: integer
  * Return: always 0 (success)
  */
+
 int _abs(int b)
 {
 	if (b >= 0)
 	{
-		 printf("%d\n", b);
+		printf("%d\n", b);
 	}
 	else if (b < 0)
 	{
