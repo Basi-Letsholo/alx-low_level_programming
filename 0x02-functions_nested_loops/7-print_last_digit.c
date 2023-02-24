530c94ccd3f8fac4ecc0193023c2dd58f60401ce
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-23 16:04:51 -0800

    fix 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_last_digit - prints last digit of integer
+ * print_last_digit - entry point
+ *
  * @n: integer
- * return: n % 10
+ * Return: n % 10
  */
 
 int print_last_digit(int n)
 {
-	printf("%d", (n % 10));
-	return (n % 10);
+	/*printf("%d", (n % 10));*/
+	int m;
+
+	if (n >= 0)
+	{
+		m = n % 10;
+		_putchar(m + 48);
+	}
+	else if (n < 0)
+	{
+		n = n - (n * 2);
+		m = n % 10;
+		_putchar(m + 48);
+	}
+	return (m);
 }
