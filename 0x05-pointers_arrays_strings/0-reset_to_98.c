0003494b2a6ca4038b40c68ac2b1f7c4b0cf10d0
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-02-27 03:48:16 -0800

    back to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - entry
+ *
+ * Description: takes a pointer to an int to update to 98
+ * @n: integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
