1d72fff1acc7de906ee0a14fff85e27577fc95f6
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-01 15:30:54 -0800

    save

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcat - entry point
+ *
+ * Description: appends string to another
+ * @dest: str added here
+ * @src: str to be added
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reverse_array - entry point
+ *
+ * Description: reverses arrays
+ * @a: array
+ * @n: length of array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, l;
+
+	l = 0;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		l++
+	}
+	for (
+}
