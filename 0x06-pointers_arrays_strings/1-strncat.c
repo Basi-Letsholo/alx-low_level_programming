30cbefcf987ab48b9b3a54c3bae222b91fd9bcfc
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-02 04:23:30 -0800

    fix strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,15 +22,21 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (j < n)
+	if (n < l)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
-	if (l < n)
+	else if (n >= l)
 	{
-		dest[i] = '\0';
+		while (src[j] != '\0')
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
 	}
+	dest[i] = '\0';
 	return (dest);
 }
