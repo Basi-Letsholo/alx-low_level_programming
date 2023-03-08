0a855b4e7828dab2587232e2a6532f7911075156
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-08 05:04:39 -0800

    rev str

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a str in reverse using recursion
+ *
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		s++;
+		_putchar(*s);
+	}
+}
