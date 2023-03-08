bce3fd2eb5874f3c580f2cb30a59e1352673aae8
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-08 02:07:31 -0800

    factorials

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,13 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	if (s[i] == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		return (0);
+	}
+	if (s[i] != '\0')
+	{
+		_putchar(_puts_recursion(s[i + 1]));
 	}
 	_putchar('\n');
 }
