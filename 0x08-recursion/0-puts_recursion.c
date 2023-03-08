75495af5c56ec2176fc2ded40cff02d57f137532
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-08 01:42:18 -0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string using recursion
+ *
+ * @s: string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
