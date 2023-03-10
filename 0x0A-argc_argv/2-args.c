4d044f1274e7991b8eedc7008c1b796a7857bdd3
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-10 07:27:15 -0800

    forgot betty again

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - prints all arguments
+ *
+ * @argc: counts number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	if (argc >= 1)
 	{
 		for (i = 0; i < argc; i++)
