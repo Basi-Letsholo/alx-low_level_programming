58bc1c247dfcbe130dd395460d77cb7124f88df3
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-10 06:43:41 -0800

    nard dog

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints number of arguments passed
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
