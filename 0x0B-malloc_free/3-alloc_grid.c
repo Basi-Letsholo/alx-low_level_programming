49238beb6b579705a1d460e292187edbf026ffbc
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-15 15:16:56 -0700

    save

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,10 +12,25 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **new = (int *new)malloc(sizeof(int) * width * height);
+	int **new = (int **)malloc(sizeof(int) * width * height);
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+	if (new)
+	{
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				new[j][i] = 0;
+			}
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new);
 }
