3243c40abf8e41efd275a23d09ad3b23dc9100a6
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-22 14:28:19 -0700

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,16 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	int x, y;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
-		printf("Error");
-		exit (98);
+		printf("Error\n");
+		exit(98);
 	}
 	else
 	{
-		num1 = atoi(argv[0]);
-		num2 = atoi(argv[2]);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		op = argv[2];
+		if (/**op == NULL ||*/ op[1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", get_op_func(op)(num1, num2));
+		return (0);
 	}
 }
