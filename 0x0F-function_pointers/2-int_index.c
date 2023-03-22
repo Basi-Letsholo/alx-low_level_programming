80bd935efb23302437180ccef6a6644bbb4ac11b
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-22 14:46:22 -0700

    null fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
+ * int_index - function that searches for a specific int
+ *
  * @array: array
  * @size: size of array
  * @cmp: pointer function
@@ -11,6 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, new, r, flag = 0;
 
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
 	if (size <= 0)
 	{
 		r = -1;
