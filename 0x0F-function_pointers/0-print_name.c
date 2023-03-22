a99c6c19b5b2abcd08cc826e11b7757844793818
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-22 08:00:03 -0700

    null fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - function that prints a name
@@ -10,5 +11,12 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name)
+	{
+		(*f)(name);
+	}
+	else
+	{
+		exit (0);
+	}
 }
