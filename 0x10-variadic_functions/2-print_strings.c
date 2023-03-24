26d5f31e2f27e72ca2afcca7ab243bed9d405503
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-24 07:21:11 -0700

    fix 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,11 +24,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if (s != NULL && i < n - 1)
+		if (s != NULL && i < n - 1 && separator != NULL)
 		{
 			printf("%s%s", s, separator);
 		}
-		if (s != NULL && i == n - 1)
+		if ((s != NULL && i == n - 1) || (separator == NULL))
 		{
 			printf("%s", s);
 		}
