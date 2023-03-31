5399f135b84ef1d764b67d229c25a0c7294a7f53
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-03-31 11:39:12 -0700

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+
+void print_first(void) __attribute__((constructor));
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
