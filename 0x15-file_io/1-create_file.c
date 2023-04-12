b2174d44ae45ec52418f2aaa47c77025e4e99af7
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-04-11 17:19:58 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 /**
  * create_file - creates a file
@@ -12,15 +14,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fw;
+	int fw;
 	ssize_t n;
+	/*char text[1000];*/
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fw = fopen(filename, O_WRONLY | O_RDONLY | O_CREAT, 600);
-	n = fwrite(fw, text_content, strlen(text_content));
-	fclose(fw);
+	fw = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	n = write(fw, text_content, sizeof(text_content));
+	if (n == -1)
+	{
+		return (-1);
+	}
 	return (1);
 }
