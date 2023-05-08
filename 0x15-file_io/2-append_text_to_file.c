c72dc455518f27422fc733351deb8ba44d3b428d
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-05-08 14:26:48 -0700

    betty fix: task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -35,14 +35,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
+
 	r = write(fd, text_content, l);
 	if (r == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	
+
 	if (close(fd) == -1)
 	{
 		return (-1);
