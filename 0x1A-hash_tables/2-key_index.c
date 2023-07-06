aeedb16c705a8f38e9e109257641a7b7c85c4c8b
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-06 15:40:21 +0200

    save

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     unsigned long int key_index;
 
-    key_index = hash_djb2(key);
+    key_index = hash_djb2(key) % size;
 
     return (key_index);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,6 +9,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int key_index;
+    unsigned long int current_index;
     hash_table_t *current_table;
+
+    current_index = key_index(k)
 }
