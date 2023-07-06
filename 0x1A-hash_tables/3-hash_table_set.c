b8f8b8427476596cccc771a49bb113df9d92267d
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-06 14:27:45 +0200

    save changes

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add to/update
+ * @key: key (non-empty string)
+ * @value: value associated with key
+ * Return: 1 on success, else 0
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int key_index;
+    hash_table_t *current_table;
+}
