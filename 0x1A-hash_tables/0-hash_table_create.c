74ec449e7f428782c82763323cd20110f7f04988
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-06 12:35:55 +0200

    Create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - creates a hash table
+* @size: size of the array
+* Return: created hash table on success, else NULL
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    char *new_hash_table[ARRAY_SIZE];
+
+    new_hash_table = malloc(sizeof(char) * size)
+    if (new_hash_table == NULL):
+        {
+            return (NULL);
+        }
+
+    return (new_hash_table);
+}
