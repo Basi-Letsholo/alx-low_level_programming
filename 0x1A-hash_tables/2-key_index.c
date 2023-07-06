cb046543b01831b98cf3b618b8a26f9f33bfe142
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-06 13:05:19 +0200

    save

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives the index of a key
+ * @size: size of the array of the hash table
+ * @key: key
+ * Return: index at which the key/value pair is stored in the hash table array
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
