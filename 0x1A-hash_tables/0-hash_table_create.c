a18c31ef07641baab7ec39e81322b105c2c71971
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-07 03:40:32 -0700

    update: malloc array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
-	hash_node_t **new_node = NULL;
+	hash_node_t **array;
 
-	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
+	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 	{
 		return (NULL);
 	}
 
+	array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
 	new_hash_table->size = size;
-	new_hash_table->array = new_node;
+	new_hash_table->array = array;
 
 	return (new_hash_table);
 }
