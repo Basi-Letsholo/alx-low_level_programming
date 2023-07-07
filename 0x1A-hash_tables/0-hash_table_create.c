b88f7c1fffdbdc95a397c8daa57f6e7dd1c20d66
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-07 03:32:42 -0700

    update: defined struct

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	hash_node_t **new_node = NULL;
 
 	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
 	if (new_hash_table == NULL)
 	{
 		return (NULL);
 	}
+
+	new_hash_table->size = size;
+	new_hash_table->array = new_node;
 
 	return (new_hash_table);
 }
