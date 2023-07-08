a8389be76a1ac9b6a2d59754b78174a8b5813dcd
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-08 10:12:00 -0700

    fixed a memory issue

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = current_node;
 	}
 
-	free(value_cp);
-	return (1);
+/*	free(value_cp);
+*/	return (1);
 
 }
