1edfe0fa64a36ca0736faaf7022c258a354d2a5d
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-09 07:54:26 -0700

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+/**
+ * linear_search - implementation of linear search alg.
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index of value location, else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
