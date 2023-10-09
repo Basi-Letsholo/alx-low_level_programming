1b46412c0f94b210783a39b4cdd9f2d1b5d468fa
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-10-09 11:32:38 -0700

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+void print_array(int *array, size_t start, size_t end);
+int binary_search_func(int *array, size_t start, size_t end, int value);
+
 #endif
